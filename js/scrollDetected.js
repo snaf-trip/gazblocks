4d627cf3cb920bb8a7c68d3263b2de1638eabe0e
Егор <egornesvickij@gmail.com>
2022-10-17 14:22:38 +0500

    Now we can open/close mobile menu.

--- js/scrollDetected.js
+++ js/scrollDetected.js
@@ -3,11 +3,14 @@ let header = document.getElementById("header");
 window.addEventListener("scroll", () => {
   console.log(window.innerWidth > 785);
 
-  if (window.scrollY !== 0 && window.innerWidth <= 785) {
-    header.style.transition = '0.5s';
-    header.style.height = '60px';
-  } else {
-    header.style.height = '80px';
+  if (window.innerWidth >= 785) {
+    if (window.scrollY !== 0) {
+      header.style.transition = '0.5s';
+      header.style.height = '60px';
+    } else {
+      header.style.height = '80px';
+    }
   }
+
 })
 
