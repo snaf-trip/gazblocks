529b614b082f471065b6f34e295c59419aa7bdfe
navzlet <sasha_zakirov_2014@mail.ru>
2022-10-21 16:31:17 +0500

    now cound hiding elements depends of window width

--- js/showMoreProducts.js
+++ js/showMoreProducts.js
@@ -1,23 +1,19 @@
 let showMoreBtn = document.querySelector(".products__showMoreBtn");
-let productsList = document.querySelector(".products__list");
 let buttonText = document.querySelector(".products__showMore_btnText");
-let hiden = document.querySelector(".hiden");
 let showMoreSvg = document.querySelector(".products__showMoreSvg");
 
-let flag = false;
+let productsList = document.querySelector(".products__list");
+let prodListArr = Array.from(productsList.children);
 
+let flag = false;
 showMoreBtn.onclick = () => {
-  console.log(flag);
   if (flag === false) {
-    productsList.style.height = "auto";
-    hiden.style.display = "flex";
-    hiden.style.flexWrap = "wrap";
-    hiden.style.justifyContent = "center";
+    prodListArr.forEach((el) => el.classList.add("isVisible"));
     buttonText.textContent = "скрыть";
     showMoreSvg.style.transform = "rotate(135deg)";
   } else {
-    productsList.style.height = "auto";
-    hiden.style.display = "none";
+    prodListArr.forEach((el) => el.classList.remove("isVisible"));
+
     buttonText.textContent = "показать ещё";
     showMoreSvg.style.transform = "rotate(225deg)";
   }
