9fb1454726ebe7a403b90db596bbff0164ca6075
navzlet <sasha_zakirov_2014@mail.ru>
2022-10-21 14:04:34 +0500

    remove height animation, svg anim by click

--- js/showMoreProducts.js
+++ js/showMoreProducts.js
@@ -2,6 +2,7 @@ let showMoreBtn = document.querySelector(".products__showMoreBtn");
 let productsList = document.querySelector(".products__list");
 let buttonText = document.querySelector(".products__showMore_btnText");
 let hiden = document.querySelector(".hiden");
+let showMoreSvg = document.querySelector(".products__showMoreSvg");
 
 let flag = false;
 
@@ -11,10 +12,12 @@ showMoreBtn.onclick = () => {
     productsList.style.height = "1054px";
     hiden.style.display = "flex";
     buttonText.textContent = "скрыть";
+    showMoreSvg.style.transform = "rotate(135deg)";
   } else {
     productsList.style.height = "527px";
     hiden.style.display = "none";
     buttonText.textContent = "показать ещё";
+    showMoreSvg.style.transform = "rotate(225deg)";
   }
   flag = !flag;
 };
