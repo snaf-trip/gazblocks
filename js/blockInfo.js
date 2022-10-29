a91341085a9ac32c1ea5ff8172ccf2aff66aa5d4
navzlet <sasha_zakirov_2014@mail.ru>
2022-10-29 22:43:47 +0500

    popup visual

--- js/blockInfo.js
+++ js/blockInfo.js
@@ -1,16 +1,23 @@
 let blocks = document.querySelectorAll(".products__item");
 let popup = document.querySelector(".popup");
 let popupContent = document.querySelector(".popup-content");
+let popupHeader = document.querySelector(".popup__header");
+let popupImg = popupContent.querySelector(".popup__img");
+let closeBtn = document.querySelector(".popup__closeSvg");
 
 for (let i = 0; i <= blocks.length; i++) {
   let block = blocks[i];
   let button = block.querySelector(".products__productInfoBtn");
   button.onclick = function () {
     popup.classList.remove("hidden");
-    popupContent.textContent = block.querySelector(
-      ".products__productName"
-    ).textContent;
-    popup.onclick = function () {
+
+    // popupHeader.textContent = block.querySelector(
+    //   ".products__productName"
+    // ).textContent;
+
+    popupImg.src = block.querySelector(".products__itemImg").src;
+
+    closeBtn.onclick = function () {
       popup.classList.add("hidden");
     };
   };
