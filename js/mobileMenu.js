546c095733e53278cf1a8fa7a69f7daa9f6c43df
Егор <egornesvickij@gmail.com>
2022-10-31 12:07:56 +0500

    Added alert and fix bugat mobile menu

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,8 +1,18 @@
 let menuBtn = document.querySelector(".header__menuButton");
 let mobileMenu = document.querySelector(".header__mobileMenu");
+let menuItems = mobileMenu.querySelectorAll(".header__mobileNavItem");
+let cross = document.querySelector(".plate4");
 let body = document.getElementById("body");
 
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("header__mobileMenu_open");
   body.classList.toggle("no-scroll");
 });
+
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", () => {
+    mobileMenu.classList.toggle("header__mobileMenu_open");
+    cross.classList.toggle("active");
+    body.classList.remove("no-scroll");
+  })
+}
